c885a6db42b29dd90aef62c96c72685457587abc
irene <irenedhierro@gmail.com>
2018-07-24 16:35:21 +0200

    Update change_id_gff.pl

--- change_id_gff.pl
+++ change_id_gff.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl -w
 
-#Convert ID script (FASTA format)
-#Usage: perl change_id_gff.pl < input.gff > output.gff
-#Requires conversion file
+# Convert ID script (FASTA format)
+# Usage: perl change_id_gff.pl < input.gff > output.gff
+# Requires conversion file
 
 use strict;
 
+# Open conversion file and store each line in an array
 open my $in, '<', "conversion_fasta_transcripts.txt";
 my @filas = <$in>;
 
@@ -13,6 +14,7 @@ my $new_id="";
 
 while (my $line =<>)
 {
+	# If the line is the gene line, grab the old id and replace it with the new id in a new file
 	chomp $line;
 	if($line=~ /^NODE\_\d+\_length\_\d+\_cov\_\d+\.?\d+\tmaker\tgene\t/)
 	{
@@ -35,11 +37,13 @@ while (my $line =<>)
 		$line =~ s/Name\=(CBGP\_AIM.*\-RA)\-gene/$new_id\-gene/g;
 		print "$line\n";
 	}
+	# Replace "maker" in every line for "new_id"
 	elsif ($line =~ /ID\=maker/g)
 	{
 		$line =~ s/ID\=(maker)/$new_id/g;
 		print "$line\n";
 	}
+	# Print any other line of the file
 	else
 	{
 		print "$line\n";

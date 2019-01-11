c0014937691f1f7193e537782fa84849d484f7ed
irene <irenedhierro@gmail.com>
2019-01-11 09:47:50 +0100

    Add files via upload

---
+++ check_presence_list.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+open(my $in, "<","new_class.txt") or die "Unable to open the file\n";
+chomp(my @nueva_uno = <$in>);
+close $in;
+my @nueva = ();
+my @vieja=();
+my %visto_antes=();
+my %visto_antes2=();
+
+#eliminar repetidos
+foreach my $uno (@nueva_uno){
+	push @nueva, $uno if not $visto_antes{$uno}++;
+}
+
+open(my $in2, "<", "old_class.txt") or die "Unable to open second file\n";
+chomp(my @vieja_dos = <$in2>);
+close $in2;
+
+foreach my $dos (@vieja_dos){
+	push @vieja, $dos if not $visto_antes2{$dos}++;
+}
+
+foreach my $new (@nueva){
+	my $count = 0;
+	foreach my $vie (@vieja){
+		if ($new eq $vie){
+			print "$new\t$vie\n";
+			$count = 1;
+		}
+	}
+	if($count == 0){
+		print "$new\tno\n";
+	}
+}
